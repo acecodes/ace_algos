dc5f90b6ca9dc005d94631ec42d3c27cd0145411
Ace <ace.github@gmail.com>
2014-11-18 12:53:01 -0800

    Restructured code into classes and staticmethods, added testing

--- algos/Python.py
+++ algos/Python.py
@@ -1,180 +1,225 @@
 # Compatability imports that allow usage in Python 2 & 3
 from __future__ import print_function
+from timeit import Timer
 
-# Linear search
-def linear_search(values, target):
-	i = 0
-	for item in values:
-		if values[i] == target:
-			return item
+class Algorithms:
+	@classmethod
+	def test(cls, algo):
+		being_tested = Timer('{0}'.format(algo))
+		return being_tested.timeit(algo)
+
+
+
+class Search(Algorithms):
+	# Linear search
+	@staticmethod
+	def linear_search(values, target):
+		i = 0
+		for item in values:
+			if values[i] == target:
+				return item
+			else:
+				i += 1
+		return -1
+
+	@staticmethod
+	# Binary search
+	def binary_search(data, target, low, high):
+
+		if low > high:
+			return False
+
 		else:
-			i += 1
-	return -1
+			mid = (low + high) // 2
+			if target == data[mid]: # Found match
+				return True
+			elif target < data[mid]:
+				# Recur on the left side of the array
+				return binary_search(data, target, low, mid-1)
+			else:
+				# Recur on the right side of the array
+				return binary_search(data, target, mid + 1, high)
 
-
-# Euclid's algorithm
-# Finds the greatest common factor (GCF) between two numbers
-def Euclid(p, q):
-	if q == 0:
-		return p
-	r = p % q
-	return Euclid(q, r)
-
-
-# Factorial (n!) - recursive
-def factorial(n):
-	if n == 0 or n == 1:
-		return 1
-	return n*factorial(n-1)
-
-# Factorial (n!) - iterative
-def factorial_i(n):
-	result = 1
-	while n > 1:
-		result = result * n
-		n -= 1
-	return result
-
-# Fibonacci (runs in quadratic time)
-def Fibonacci(n):
-	if n <= 1:
-		return n
-	return Fibonacci(n-1) + Fibonacci(n-2)
-
-# Fibonacci (runs in linear time)
-def Fibonacci_improved(n):
-	if n <= 1:
-		return (n, 0)
-	else:
-		(a,b) = Fibonacci_improved(n-1)
-		return (a+b, a)
-
-# Merge sort
-def merge_sort(array):
-	if len(array) == 1 or len(array) == 0:
-		return array
-	midpoint = len(array)/2
-	left_side = array[:midpoint]
-	right_side = array[midpoint:]
-
-	merge_sort(left_side)
-	merge_sort(right_side)
-
-	left_count = 0
-	right_count = 0
-	sorted_count = 0
-
-	while left_count < len(left_side) and right_count < len(right_side):
-		if left_side[left_count] < right_side[right_count]:
-			array[sorted_count] = left_side[left_count]
-			left_count += 1
-		else:
-			array[sorted_count] = right_side[right_count]
-			right_count += 1
-		sorted_count += 1
-
-	while left_count < len(left_side):
-		array[sorted_count] = left_side[left_count]
-		left_count += 1
-		sorted_count += 1
-
-	while right_count < len(right_side):
-		array[sorted_count] = right_side[right_count]
-		right_count += 1
-		sorted_count += 1
-
-	return array
-
-# Luhn's algorith, for generating and validating number sequences
-# AKA mod 10 algorithm
-
-# Generate a proper sum for a given sequence
-def Luhn(digits):
-	# Ensure that the data entered can become a list
-	if type(digits) != 'str':
-		digits = str(digits)
-	
-	# Convert data entered 
-	digits = [int(x) for x in digits]
-
-	# Run heart of the algorithm, a description of which can be found on Wikipedia:
-	# https://en.wikipedia.org/wiki/Luhn_algorithm
-
-	for chars in digits[-1::-2]:
-		if chars*2 > 9:
-			subscript = digits.index(chars)
-			chars = chars*2
-			chars = int(int(str(chars)[0])+int(str(chars)[1]))
-			digits[subscript] = chars
-		else:
-			subscript = digits.index(chars)
-			chars = chars*2
-			digits[subscript] = chars
-
-	return sum(digits)
-
-# Generate a check digit
-def Luhn_digit(digits):
-	return str(Luhn(digits)*9)[-1]
-
-# See if a sequence of digits and a key match up
-def Luhn_check(digits, check):
-	return (Luhn(digits) + check) % 10 == 0
-
-# Recursively sum up a list of numbers
-def listsum(lst):
-	# Define a base case
-	if len(lst) == 1:
-		return lst[0]
-	# Move through the list recursively
-	else:
-		return lst[0] + listsum(lst[1:])
-
-# Binary search
-def binary_search(data, target, low, high):
-
-	if low > high:
+	@staticmethod
+	# Binary search with iteration
+	def binary_search_iter(data, target):
+		"""Return True if target is found in the given list"""
+		low = 0
+		high = len(data)-1
+		while low <= high:
+			mid = (low+high) // 2
+			if target == data[mid]:
+				return True
+			elif target < data[mid]:
+				high = mid - 1
+			else:
+				low = mid + 1
 		return False
 
-	else:
-		mid = (low + high) // 2
-		if target == data[mid]: # Found match
-			return True
-		elif target < data[mid]:
-			# Recur on the left side of the array
-			return binary_search(data, target, low, mid-1)
+
+
+class Math(Algorithms):
+	# Euclid's algorithm
+	# Finds the greatest common factor (GCF) between two numbers
+	@staticmethod
+	def Euclid(p, q):
+		if q == 0:
+			return p
+		r = p % q
+		return Euclid(q, r)
+
+
+	# Factorial (n!) - recursive
+	@staticmethod
+	def factorial(n):
+		if n == 0 or n == 1:
+			return 1
+		return n*Math.factorial(n-1)
+
+	# Factorial (n!) - iterative
+	@staticmethod
+	def factorial_i(n):
+		result = 1
+		while n > 1:
+			result = result * n
+			n -= 1
+		return result
+
+	# Fibonacci (runs in quadratic time)
+	@staticmethod
+	def Fibonacci(n):
+		if n <= 1:
+			return n
+		return Fibonacci(n-1) + Fibonacci(n-2)
+
+	# Fibonacci (runs in linear time)
+	@staticmethod
+	def Fibonacci_improved(n):
+		if n <= 1:
+			return (n, 0)
 		else:
-			# Recur on the right side of the array
-			return binary_search(data, target, mid + 1, high)
+			(a,b) = Fibonacci_improved(n-1)
+			return (a+b, a)
 
-# Binary search with iteration
-def binary_search_iter(data, target):
-	"""Return True if target is found in the given list"""
-	low = 0
-	high = len(data)-1
-	while low <= high:
-		mid = (low+high) // 2
-		if target == data[mid]:
-			return True
-		elif target < data[mid]:
-			high = mid - 1
+	@staticmethod
+	def sum_of_nums_from_string(string):
+		return sum([float(x) for x in string.split(',')])
+
+	# Recursively sum up a list of numbers
+	@staticmethod
+	def listsum(lst):
+		# Define a base case
+		if len(lst) == 1:
+			return lst[0]
+		# Move through the list recursively
 		else:
-			low = mid + 1
-	return False
+			return lst[0] + listsum(lst[1:])
 
-def reverse(s):
-	"""Iteratively reverse items in sequence s"""
-	start, stop = 0, len(s)
-	while start < stop - 1:
-		s[start], s[stop-1] = s[stop-1], s[start]
-		start, stop = start + 1, stop - 1
-	return s
+class Sorting(Algorithms):
+	# Merge sort
+	@staticmethod
+	def merge_sort(array):
+		if len(array) == 1 or len(array) == 0:
+			return array
+		midpoint = len(array)/2
+		left_side = array[:midpoint]
+		right_side = array[midpoint:]
 
-def sum_of_nums_from_string(string):
-	return sum([float(x) for x in string.split(',')])
+		merge_sort(left_side)
+		merge_sort(right_side)
 
-# Test array for sorting algorithms
-test_array = [4, 5, 9, 1, 3, 2, 12, 7]
+		left_count = 0
+		right_count = 0
+		sorted_count = 0
+
+		while left_count < len(left_side) and right_count < len(right_side):
+			if left_side[left_count] < right_side[right_count]:
+				array[sorted_count] = left_side[left_count]
+				left_count += 1
+			else:
+				array[sorted_count] = right_side[right_count]
+				right_count += 1
+			sorted_count += 1
+
+		while left_count < len(left_side):
+			array[sorted_count] = left_side[left_count]
+			left_count += 1
+			sorted_count += 1
+
+		while right_count < len(right_side):
+			array[sorted_count] = right_side[right_count]
+			right_count += 1
+			sorted_count += 1
+
+		return array
+
+	# A simple insertion sort (O(n^2)) algorithm
+	@staticmethod
+	def insertion_sort(q):
+		for i in range(1, len(q)):
+			current = q[i]
+			j = i
+			while j > 0 and q[j-1] > current:
+				q[j] = q[j-1]
+				j -= 1
+			q[j] = current
+
+class Generation(Algorithms):
+	# Luhn's algorith, for generating and validating number sequences
+	# AKA mod 10 algorithm
+
+	# Generate a proper sum for a given sequence
+	@staticmethod
+	def Luhn(digits):
+		# Ensure that the data entered can become a list
+		if type(digits) != 'str':
+			digits = str(digits)
+		
+		# Convert data entered 
+		digits = [int(x) for x in digits]
+
+		# Run heart of the algorithm, a description of which can be found on Wikipedia:
+		# https://en.wikipedia.org/wiki/Luhn_algorithm
+
+		for chars in digits[-1::-2]:
+			if chars*2 > 9:
+				subscript = digits.index(chars)
+				chars = chars*2
+				chars = int(int(str(chars)[0])+int(str(chars)[1]))
+				digits[subscript] = chars
+			else:
+				subscript = digits.index(chars)
+				chars = chars*2
+				digits[subscript] = chars
+
+		return sum(digits)
+
+	# Generate a check digit
+	@staticmethod
+	def Luhn_digit(digits):
+		return str(Generation.Luhn(digits)*9)[-1]
+
+	# See if a sequence of digits and a key match up
+	@staticmethod
+	def Luhn_check(digits, check):
+		return (Generation.Luhn(digits) + check) % 10 == 0
+
+	@staticmethod
+	def combos(n, permutations):
+		"""Return combinations from a set of numbers up to n"""
+		from itertools import combinations
+		return list(combinations(range(n), permutations))
+
+	@staticmethod
+	def reverse(s):
+		"""Iteratively reverse items in sequence s"""
+		start, stop = 0, len(s)
+		while start < stop - 1:
+			s[start], s[stop-1] = s[stop-1], s[start]
+			start, stop = start + 1, stop - 1
+		return s
+
+
 
 def measure_dynamic_array(n):
 	"""Measure the number of bytes a dynamic array up to size n-1
@@ -187,25 +232,9 @@ def measure_dynamic_array(n):
 		print("Length: {0:3d}; Size in bytes: {1:4d}".format(a,b))
 		data.append(None)
 
-def combos(n, permutations):
-	"""Return combinations from a set of numbers up to n"""
-	from itertools import combinations
-	return list(combinations(range(n), permutations))
-
 
 if __name__ == '__main__':
-	print(Luhn_digit(7992739871)) # Generate a valid key sum for a check digit
-	print(Luhn_check(7992739871, 3)) # Check a valid digit and key - should return True
-	print(Luhn_check(7992739871, 2)) # Check an invalid digit and key - should return False
-	print(listsum([1,56,9,12,22])) # Should return 100
-	print(linear_search([5, 1, 8, 15, 29, 3, 19], 3)) # Returns 3
-	print(linear_search([5, 1, 8, 15, 29, 3, 19], 27)) # Returns -1
-	print(binary_search(test_array, 3, 1, 12))
-	print(binary_search_iter(test_array, 3))
-	print(sum_of_nums_from_string("1.23,1.10,5.9,8.1"))
-	print(Fibonacci_improved(10))
-	print(reverse(test_array))
-	print(measure_dynamic_array(20)) # Note how an empty dynamic array takes up 72 (on my system) bytes
-	print(combos(5, 3))
-	print(factorial(5)) # Should return 120
-	print(factorial_i(5)) # Should also return 120
+	print(Math.test(Math.factorial(10))) # Test speed of factorial function
+	print(Generation.Luhn_digit(7992739871)) # Generate a valid key sum for a check digit
+	print(Generation.Luhn_check(7992739871, 3)) # Check a valid digit and key - should return True
+	print(Generation.Luhn_check(7992739871, 2)) # Check an invalid digit and key - should return False
