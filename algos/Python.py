5bf76bc23d55153f82186421f2aa36d3aac83156
Ace <ace.github@gmail.com>
2015-02-01 11:40:21 -0800

    First bits of a linked list implementation in Python

--- algos/Python.py
+++ algos/Python.py
@@ -149,6 +149,73 @@ class DataStructures(Algorithms):
 
             return still_equal
 
+    class Node:
+        """Node for linked list implementation"""
+        def __init__(self, init_data):
+            self.data = init_data
+            self.next = None
+
+        def get_data(self):
+            return self.data
+
+        def get_next(self):
+            return self.next
+
+        def set_data(self, new_data):
+            self.data = new_data
+
+        def set_next(self, new_next):
+            self.next = new_next
+
+    class UnorderedList:
+        """Unordered (linked) list"""
+        def __init__(self):
+            self.head = None
+
+        def is_empty(self):
+            return self.head is None
+
+        def add(self, item):
+            node = DataStructures.Node(item)
+            node.set_next(self.head)
+            self.head = node
+
+        def size(self):
+            current = self.head
+            count = 0
+            while current is not None:
+                count = count + 1
+                current = current.get_next()
+
+            return count
+
+        def search(self, item):
+            current = self.head
+            found = False
+            while current is not None and not found:
+                if current.get_data() == item:
+                    found = True
+                else:
+                    current = current.get_next()
+
+            return found
+
+        def remove(self, item):
+            current = self.head
+            previous = None
+            found = False
+            while not found:
+                if current.get_data() == item:
+                    found = True
+                else:
+                    previous = current
+                    current = current.get_next()
+
+            if previous == None:
+                self.head = current.get_next()
+            else:
+                previous.set_next(current.get_next())
+
 
 class Search(Algorithms):
     # Linear search
@@ -476,3 +543,13 @@ if __name__ == '__main__':
     deque2 = DataStructures.Deque()
     print(deque2.palindrome_checker('radar'))
     print(deque2.palindrome_checker('steve'))
+
+    """Linked list"""
+    linked1 = DataStructures.UnorderedList()
+    linked1.add(15)
+    linked1.add(22)
+    print(linked1.size())
+    print(linked1.search(22))
+    print(linked1.search(2))
+    linked1.remove(15)
+    print(linked1.size())
