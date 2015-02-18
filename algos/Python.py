caa98ea9de9a850ccc141c40ecc4c481bb1d6bf2
Ace <ace.github@gmail.com>
2015-02-18 10:51:07 -0800

    Binary search tree for Python

--- algos/Python.py
+++ algos/Python.py
@@ -529,6 +529,250 @@ class DataStructures(Algorithms):
             self.move_down(1)
             return return_value
 
+    class TreeNode:
+        """Node for binary search tree"""
+
+        def __init__(self, key, value,
+                     left=None, right=None, parent=None):
+
+            self.key = key
+            self.payload = value
+            self.left_child = left
+            self.right_child = right
+            self.parent = parent
+
+        def __iter__(self):
+            if self:
+                if self.has_left_child():
+                    for element in self.left_child:
+                        yield element
+                yield self.key
+                if self.has_right_child():
+                    for element in self.right_child:
+                        yield element
+
+        def has_left_child(self):
+            return self.left_child
+
+        def has_right_child(self):
+            return self.right_child
+
+        def is_left_child(self):
+            return self.parent and self.parent.left_child == self
+
+        def is_right_child(self):
+            return self.parent and self.parent.right_child == self
+
+        def is_root(self):
+            return not self.parent
+
+        def is_leaf(self):
+            return not (self.right_child or self.left_child)
+
+        def has_any_children(self):
+            return self.right_child or self.left_child
+
+        def has_both_children(self):
+            return self.right_child and self.left_child
+
+        def replace_node_data(self, key, value, left_child, right_child):
+            self.key = key
+            self.payload = value
+            self.left_child = left_child
+            self.right_child = right_child
+            if self.has_left_child():
+                self.left_child.parent = self
+            if self.has_right_child():
+                self.right_child.parent = self
+
+    class BinarySearchTree:
+        """Binary search tree - methods with single underscores are helpers"""
+
+        def __init__(self):
+            self.root = None
+            self.size = 0
+
+        def length(self):
+            return self.size
+
+        def __len__(self):
+            return self.size
+
+        def __iter__(self):
+            return self.root.__iter__()
+
+        def __setitem__(self, key, value):
+            """
+            Enables adding via dictionary syntax:
+            my_tree['boom'] = 5
+            """
+            self.put(key, value)
+
+        def __getitem__(self, key):
+            """
+            Enables dictionary lookup syntax:
+            my_tree['boom']
+            """
+            return self.get(key)
+
+        def __contains__(self, key):
+            """
+            Enables in calls:
+            if 'boom' in my_tree: ...
+            """
+            if self._get(key, self.root):
+                return True
+            else:
+                return False
+
+        def __delitem__(self, key):
+            """
+            Enables use of 'del' keyword:
+            del my_tree['boom']
+            """
+            self.delete(key)
+
+        def put(self, key, value):
+            if self.root:
+                self._put(key, value, self.root)
+            else:
+                self.root = DataStructures.TreeNode(key, value)
+            self.size = self.size + 1
+
+        def _put(self, key, value, current_node):
+            if key < current_node.key:
+                if current_node.has_left_child():
+                    self._put(key, value, current_node.left_child)
+                else:
+                    current_node.left_child = DataStructures.TreeNode(
+                                              key, value, parent=current_node)
+            else:
+                if current_node.has_right_child():
+                    self._put(key, value, current_node.right_child)
+                else:
+                    current_node.right_child = DataStructures.TreeNode(
+                                               key, value, parent=current_node)
+
+        def get(self, key):
+            if self.root:
+                res = self._get(key, self.root)
+                if res:
+                    return res.payload
+                else:
+                    return None
+
+            else:
+                return None
+
+        def _get(self, key, current_node):
+            if not current_node:
+                return None
+            elif current_node.key == key:
+                return current_node
+            elif key < current_node.key:
+                return self._get(key, current_node.left_child)
+            else:
+                return self._get(key, current_node.right_child)
+
+        def delete(self, key):
+            if self.size > 1:
+                node_to_remove = self._get(key, self.root)
+                if node_to_remove:
+                    self.remove(node_to_remove)
+                    self.size = self.size - 1
+                else:
+                    raise KeyError('Key not in tree')
+            elif self.size == 1 and self.root.key == key:
+                self.root = None
+                self.size = self.size - 1
+            else:
+                raise KeyError('Key not in tree')
+
+        def find_successor(self):
+            successor = None
+            if self.has_right_child():
+                successor = self.right_child.find_min()
+            else:
+                if self.parent:
+                    if self.is_left_child():
+                        successor = self.parent
+                    else:
+                        self.parent.right_child = None
+                        successor = self.parent.find_successor()
+                        self.parent.right_child = self
+
+            return successor
+
+        def find_min(self):
+            current = self
+            while current.has_left_child():
+                current = current.left_child
+            return current
+
+        def splice_out(self):
+            if self.is_leaf():
+                if self.is_left_child():
+                    self.parent.left_child = None
+                else:
+                    self.parent.right_child = None
+            elif self.has_any_children():
+                if self.has_left_child():
+                    if self.is_left_child():
+                        self.parent.left_child = self.left_child
+                    else:
+                        self.parent.right_child = self.left_child
+                    self.left_child.parent = self.parent
+            else:
+                if self.is_left_child():
+                    self.parent.left_child = self.right_child
+                else:
+                    self.parent.right_child = self.right_child
+                self.right_child.parent = self.parent
+
+        def remove(self, current_node):
+            if current_node.is_leaf():
+                """Node is a leaf (childless)"""
+                if current_node == current_node.parent.left_child:
+                    current_node.parent.left_child = None
+                else:
+                    current_node.parent.right_child = None
+            elif current_node.has_both_children():
+                sucessor = current_node.find_successor()
+                sucessor.splice_out()
+                current_node.key = sucessor.key
+                current_node.payload = sucessor.payload
+
+            else:
+                """Node has one child"""
+                if current_node.has_left_child():
+                    if current_node.is_left_child():
+                        current_node.left_child.parent = current_node.parent
+                        current_node.parent.left_child = current_node.left_child
+                    elif current_node.is_right_child():
+                        current_node.left_child.parent = current_node.parent
+                        current_node.parent.right_child = current_node.left_child
+                    else:
+                        current_node.replace_node_data(
+                            current_node.left_child.key,
+                            current_node.left_child.payload,
+                            current_node.left_child.left_child,
+                            current_node.left_child.right_child
+                            )
+                else:
+                    if current_node.is_left_child():
+                        current_node.right_child.parent = current_node.parent
+                        current_node.parent.left_child = current_node.right_child
+                    elif current_node.is_right_child():
+                        current_node.right_child.parent = current_node.parent
+                        current_node.parent.right_child = current_node.right_child
+                    else:
+                        current_node.replace_node_data(
+                            current_node.right_child.key,
+                            current_node.right_child.payload,
+                            current_node.right_child.left_child,
+                            current_node.right_child.right_child,
+                            )
+
 
 class Search(Algorithms):
 
@@ -1022,7 +1266,17 @@ if __name__ == '__main__':
     # parse_tree1 = DataStructures.BinaryTree.build_parse_tree("( ( 10 + 5 ) * 3 )")
     # print(DataStructures.BinaryTree.evaluate(parse_tree1))  # 45
 
-    """Binary min heap"""
-    min_heap = DataStructures.BinHeap()
-    min_heap.build_heap([5, 8, 2, 4, 9, 12])
-    print(min_heap.heap_list)
+    # """Binary min heap"""
+    # min_heap = DataStructures.BinHeap()
+    # min_heap.build_heap([5, 8, 2, 4, 9, 12])
+    # print(min_heap.heap_list)
+
+    """Binary search tree"""
+    bst = DataStructures.BinarySearchTree()
+    bst[3] = "Zebra"
+    bst[4] = "Cat"
+    bst[6] = "Antelope"
+
+    print(bst[3])
+    print(bst[4])
+    print(bst[6])
