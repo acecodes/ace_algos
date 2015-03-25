44650513267e03489d4f1bd8b2d76314fa872248
Ace <ace.github@gmail.com>
2015-03-25 15:27:13 -0700

    Linked list for JavaScript

--- algos/JavaScript.js
+++ algos/JavaScript.js
@@ -80,3 +80,97 @@ var Hanoi = function Hanoi(disk, src, aux, dst) {
   }
 
 };
+
+// Linked list
+var firstNode = {
+  data: 0,
+  next: null
+};
+
+// Attach first node, creating the list
+firstNode.next = {
+  data: 1,
+  next: null
+};
+
+// Linked list constructor
+function LinkedList() {
+  this._length = 0;
+  this._head = null;
+}
+
+// Linked list methods
+LinkedList.prototype = {
+  
+  // Create new node with data
+  add: function(data) {
+    var node = {
+      data: data,
+      next: null
+    },
+
+    current;
+
+    if (this._head === null) {
+      this._head = node;
+    } else {
+      current = this._head;
+      
+      while (current.next) {
+        current = current.next;
+      }
+
+      current.next = node;
+    }
+
+    this._length++;
+  },
+
+  // Check for item at index, return null if empty
+  item: function(index) {
+    //Check for out-of-bounds values
+    if (index > -1 && index < this._length) {
+      var current = this._head,
+      i = 0;
+
+      while (i++ < index) {
+        current = current.next;
+      }
+      return current.data;
+    } else {
+      return null;
+    }
+  },
+
+  remove: function(index) {
+    if (index > -1 && index < this._length) {
+      var current = this._head, 
+      previous, 
+      i=0;
+
+      if (index === 0) {
+        this._head = current.next;
+      } else {
+        while (i++ < index) {
+          previous = current;
+          current = current.next;
+        }
+
+        previous.next = current.next;
+      }
+
+      this._length--;
+
+      return current.data;
+    } else {
+      return null;
+    }
+  }
+};
+
+var ll = new LinkedList();
+ll.add(5);
+ll.add(10);
+console.log(ll.item(0)); // 5
+ll.remove(0);
+console.log(ll.item(0)); // 10
