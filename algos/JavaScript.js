a62f07d750d5502f0781db1e52f87459338c26ca
Ace <ace.github@gmail.com>
2015-03-27 14:49:48 -0700

    Cleaned up JavaScript formatting for algos, added Codility maximum triplets algorithm for JS

--- algos/JavaScript.js
+++ algos/JavaScript.js
@@ -1,23 +1,30 @@
 // Euclid's algorithm for finding GCFs
 var Euclid = function Euclid(p, q) {
-	if (q === 0) { return p; }
-	else {
-		var r = p % q;
-		return Euclid(q, r);
-	}
+    if (q === 0) {
+        return p;
+    } else {
+        var r = p % q;
+        return Euclid(q, r);
+    }
 
 };
 
 // Factorial
 var factorial = function factorial(n) {
-    if (n === 1 || n === 0) { return 1; }
-    else { return n*factorial(n-1); }
+    if (n === 1 || n === 0) {
+        return 1;
+    } else {
+        return n * factorial(n - 1);
+    }
 };
 
 // Fibonacci series
 var fibonacci = function fibonacci(n) {
-	if (n <= 1) { return n; }
-	else { return fibonacci(n-1) + fibonacci(n-2); }
+    if (n <= 1) {
+        return n;
+    } else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
 
 };
 
@@ -37,143 +44,135 @@ var fibonacciMemo = (function() {
 })();
 
 // Memoizer for usage in other recursive algorithms
-var memoizer = function (memo, formula) {
-  var recur = function (n) {
-    var result = memo[n];
-    if (typeof result !== 'number') {
-      result = formula(recur, n);
-      memo[n] = result;
-    }
-    return result;
-  };
-  return recur;
+var memoizer = function(memo, formula) {
+    var recur = function(n) {
+        var result = memo[n];
+        if (typeof result !== 'number') {
+            result = formula(recur, n);
+            memo[n] = result;
+        }
+        return result;
+    };
+    return recur;
 };
 
 
 // Fibonacci and factorial utilizing the memoizer function
-var fibonacciShortMemo = memoizer([0, 1], function (recur, n) {
-  return recur(n-1) + recur(n-2);
+var fibonacciShortMemo = memoizer([0, 1], function(recur, n) {
+    return recur(n - 1) + recur(n - 2);
 });
 
-var factorialShortMemo = memoizer([1, 1], function (recur, n) {
-  return n * recur(n-1);
+var factorialShortMemo = memoizer([1, 1], function(recur, n) {
+    return n * recur(n - 1);
 });
 
 // Palindrome checker
 var palindrome = function palindrome(str) {
-  newstr = str.toLowerCase();
-  if (newstr[0] !== newstr.slice(-1)) {
-    return false;
-  }
-  else {
-    return true;
-  }
-   return palindrome(newstr[0]) + palindrome(newstr[newstr.slice(-1)]);
+    newstr = str.toLowerCase();
+    if (newstr[0] !== newstr.slice(-1)) {
+        return false;
+    } else {
+        return true;
+    }
+    return palindrome(newstr[0]) + palindrome(newstr[newstr.slice(-1)]);
 };
 
 // Tower of Hanoi solver
 var Hanoi = function Hanoi(disk, src, aux, dst) {
-  if (disk > 0) {
-    Hanoi(disk - 1, src, dst, aux);
-    console.log('Move disk ' + disk + ' from ' + src + ' to ' + dst);
-    Hanoi(disk - 1, aux, src, dst);
-  }
+    if (disk > 0) {
+        Hanoi(disk - 1, src, dst, aux);
+        console.log('Move disk ' + disk + ' from ' + src + ' to ' + dst);
+        Hanoi(disk - 1, aux, src, dst);
+    }
 
 };
 
 // Linked list
 var firstNode = {
-  data: 0,
-  next: null
+    data: 0,
+    next: null
 };
 
 // Attach first node, creating the list
 firstNode.next = {
-  data: 1,
-  next: null
+    data: 1,
+    next: null
 };
 
 // Linked list constructor
 function LinkedList() {
-  this._length = 0;
-  this._head = null;
+    this._length = 0;
+    this._head = null;
 }
 
 // Linked list methods
 LinkedList.prototype = {
-  
-  // Create new node with data
-  add: function(data) {
-    var node = {
-      data: data,
-      next: null
-    },
 
-    current;
+    // Create new node with data
+    add: function(data) {
+        var node = {
+                data: data,
+                next: null
+            },
 
-    if (this._head === null) {
-      this._head = node;
-    } else {
-      current = this._head;
-      
-      while (current.next) {
-        current = current.next;
-      }
+            current;
 
-      current.next = node;
-    }
+        if (this._head === null) {
+            this._head = node;
+        } else {
+            current = this._head;
 
-    this._length++;
-  },
+            while (current.next) {
+                current = current.next;
+            }
 
-  // Check for item at index, return null if empty
-  item: function(index) {
-    //Check for out-of-bounds values
-    if (index > -1 && index < this._length) {
-      var current = this._head,
-      i = 0;
-
-      while (i++ < index) {
-        current = current.next;
-      }
-      return current.data;
-    } else {
-      return null;
-    }
-  },
-
-  remove: function(index) {
-    if (index > -1 && index < this._length) {
-      var current = this._head, 
-      previous, 
-      i=0;
-
-      if (index === 0) {
-        this._head = current.next;
-      } else {
-        while (i++ < index) {
-          previous = current;
-          current = current.next;
+            current.next = node;
         }
 
-        previous.next = current.next;
-      }
+        this._length++;
+    },
 
-      this._length--;
+    // Check for item at index, return null if empty
+    item: function(index) {
+        //Check for out-of-bounds values
+        if (index > -1 && index < this._length) {
+            var current = this._head,
+                i = 0;
 
-      return current.data;
-    } else {
-      return null;
+            while (i++ < index) {
+                current = current.next;
+            }
+            return current.data;
+        } else {
+            return null;
+        }
+    },
+
+    remove: function(index) {
+        if (index > -1 && index < this._length) {
+            var current = this._head,
+                previous,
+                i = 0;
+
+            if (index === 0) {
+                this._head = current.next;
+            } else {
+                while (i++ < index) {
+                    previous = current;
+                    current = current.next;
+                }
+
+                previous.next = current.next;
+            }
+
+            this._length--;
+
+            return current.data;
+        } else {
+            return null;
+        }
     }
-  }
 };
-
-// var ll = new LinkedList();
-// ll.add(5);
-// ll.add(10);
-// console.log(ll.item(0)); // 5
-// ll.remove(0);
-// console.log(ll.item(0)); // 10
 
 // Array flattener
 function flattenArrays(a, r) {
@@ -188,4 +187,28 @@ function flattenArrays(a, r) {
         }
     }
     return r;
+}
+
+// Find maximum triplet, from Codility
+function solution(A) { // A is an array
+
+    Array.prototype.max = function() {
+        return Math.max.apply(null, this);
+    };
+
+    if (A.length < 3) {
+        throw "The array is too small";
+    }
+
+    function sorter(a, b) {
+        return a - b;
+    }
+
+    A = A.sort(sorter);
+
+    var result = [A[0] * A[1] * A[A.length - 1],
+        A[A.length - 1] * A[A.length - 2] * A[A.length - 3]
+    ].max();
+
+    return result;
 }
