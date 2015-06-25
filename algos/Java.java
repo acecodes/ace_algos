d0341bbeb792e67f82ec711ae14de45302fe5359
Ace <ace.github@gmail.com>
2015-06-25 12:55:41 -0700

    Added binary search to Java

--- algos/Java.java
+++ algos/Java.java
@@ -11,6 +11,31 @@ public class Java {
     return -1;
   }
 
+  private static int binary_search(int[] array, int target) {
+    // Binary search - fastest possible search, needs a sorted collection
+    int first = 0;
+    int last = array.length - 1;
+    int middle = (first + last) / 2;
+    int pos = -1;
+
+    while (first <= last) {
+      if (array[middle] < target) {
+        first = middle + 1;
+      }
+      else if (array[middle] == target) {
+        pos = middle;
+        return pos;
+      }
+      else {
+        last = middle - 1;
+      }
+
+      middle = (first + last) / 2;
+    }
+
+    return pos;
+  }
+
   private static int factorial(int n) {
     // Recursive factorial
     if (n <= 0) {
@@ -20,11 +45,12 @@ public class Java {
     }
   }
 
-
   public static void main(String[] args) {
     int[] test_array = {1, 5, 12, 9, 2, 3, 6, 21};
+    int[] sorted_array = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
 
     System.out.println(linear_search(test_array, 9));
     System.out.println(factorial(5));
+    System.out.println(binary_search(sorted_array, 3));
   }
 }
