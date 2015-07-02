e50ccce49b01d479dae082b95b91c634f83597dc
Ace <ace.github@gmail.com>
2015-07-01 19:18:17 -0700

    Factorial for Rust

--- algos/Rust.rs
+++ algos/Rust.rs
@@ -6,6 +6,15 @@ pub fn fibonacci(n: i32) -> i32 {
 	}
 }
 
+pub fn factorial(n: i32) -> i32 {
+    if n <= 1 {
+        return 1;
+    }
+
+    return n * factorial(n-1);
+}
+
 fn main() {
 	println!("Fibonacci (5): {}", fibonacci(5));
+    println!("Factorial (5): {}", factorial(5));
 }
