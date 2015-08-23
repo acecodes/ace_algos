e88e30d96c59cc59a6cd69be7d54184235bd9d2e
Ace <ace.github@gmail.com>
2015-08-23 08:12:22 -0700

    Refactored JS code into easier-to-read prototypes

--- algos/JavaScript.js
+++ algos/JavaScript.js
@@ -1,59 +1,150 @@
 var Main = (function() {
+    
     "use strict";
-    // Euclid's algorithm for finding GCFs
-    function euclid(p, q) {
-        if (q === 0) {
-            return p;
-        } else {
-            var r = p % q;
-            return euclid(q, r);
-        }
-    }
-    // Recursively sum up to n
-    function summer(n) {
-        if (n > 0) {
-            return n + summer(n - 1);
-        } else {
-            return 0;
-        }
-    }
-    // Recursively calculate power of n
-    function powers(n, power) {
-        if (power == 1) {
-            return n;
-        } else {
-            return n * powers(n, power - 1);
-        }
-    }
-    // Factorial
-    function factorial(n) {
-        if (n === 1 || n === 0) {
-            return 1;
-        } else {
-            return n * factorial(n - 1);
-        }
-    }
-    // Fibonacci series
-    function fibonacci(n) {
-        if (n <= 1) {
-            return n;
-        } else {
-            return fibonacci(n - 1) + fibonacci(n - 2);
-        }
-    }
-    // Fibonacci with memoization
-    function fibonacciMemo() {
-        var memo = [0, 1];
-        var fib = function(n) {
-            var result = memo[n];
-            if (typeof result !== "number") {
-                result = fib(n - 1) + fib(n - 2);
-                memo[n] = result;
+
+    function Recursion() {}
+
+    Recursion.prototype = {
+        euclid: function euclid(p, q) {
+            // Euclid's algorithm for finding GCFs
+            if (q === 0) {
+                return p;
+            } else {
+                var r = p % q;
+                return euclid(q, r);
             }
-            return result;
-        };
-        return fib;
-    }
+        },
+
+        summer: function summer(n) {
+            // Recursively sum up to n
+            if (n > 0) {
+                return n + summer(n - 1);
+            } else {
+                return 0;
+            }
+        },
+
+        powers: function powers(n, power) {
+            // Recursively calculate power of n
+            if (power == 1) {
+                return n;
+            } else {
+                return n * powers(n, power - 1);
+            }
+        },
+
+        factorial: function factorial(n) {
+            // Factorial
+            if (n === 1 || n === 0) {
+                return 1;
+            } else {
+                return n * factorial(n - 1);
+            }
+        },
+
+        fibonacci: function fibonacci(n) {
+            // Fibonacci series
+            if (n <= 1) {
+                return n;
+            } else {
+                return fibonacci(n - 1) + fibonacci(n - 2);
+            }
+        },
+
+        fibonacciMemo: function fibonacciMemo() {
+            // Fibonacci with memoization
+            var memo = [0, 1];
+            var fib = function(n) {
+                var result = memo[n];
+                if (typeof result !== "number") {
+                    result = fib(n - 1) + fib(n - 2);
+                    memo[n] = result;
+                }
+                return result;
+            };
+            return fib;
+        },
+
+        hanoi: function hanoi(disk, src, aux, dst) {
+            // Tower of Hanoi solver
+            if (disk > 0) {
+                hanoi(disk - 1, src, dst, aux);
+                console.log("Move disk " + disk + " from " + src + " to " + dst);
+                hanoi(disk - 1, aux, src, dst);
+            }
+        },
+
+        palindrome: function palindrome(str) {
+            // Palindrome checker
+            newstr = str.toLowerCase();
+            if (newstr[0] !== newstr.slice(-1)) {
+                return false;
+            } else {
+                return true;
+            }
+            return palindrome(newstr[0]) + palindrome(newstr[newstr.slice(-1)]);
+        }
+    };
+
+    function Search() {}
+
+    Search.prototype = {
+        linearSearch: function linearSearch(array, target) {
+            // Linear search
+            var len = array.length;
+            for (var i = 0; i < len; i++) {
+                if (array[i] === target) {
+                    return i;
+                }
+            }
+            return -1;
+        }
+    };
+
+    function Sorting() {}
+
+    Sorting.prototype = {
+        mergeSort: function mergeSort(array, start, end) {
+            if (start < end) {
+                var mid = Math.floor((start + end) / 2);
+                mergeSort(array, start, mid);
+                mergeSort(array, mid + 1, end);
+                merge(array, start, mid, end); // Implement merge
+            }
+
+            // Merge, which performs most of the work in merge sort
+            function merge(left, right) {
+                var result = [],
+                    indexLeft = 0,
+                    indexRight = 0,
+                    leftLength = left.length,
+                    rightLength = right.length;
+
+                while (indexLeft < leftLength && indexRight < rightLength) {
+                    if (left[indexLeft] < right[indexRight]) {
+                        result.push(left[indexLeft++]);
+                    } else {
+                        result.push(right[indexRight++]);
+                    }
+                }
+                return result.concat(left.slice(indexLeft).concat(right.slice(indexRight)));
+            }
+            // Merge sort, recursive sorting algorithm with guaranteed n log n performance
+            function mergeSort(array) {
+                var len = array.length;
+                if (len < 2) {
+                    return array;
+                }
+
+                var middle = Math.floor(len / 2),
+                    left = array.slice(0, middle),
+                    right = array.slice(middle);
+
+                return merge(mergeSort(left), mergeSort(right));
+            }
+        }
+    };
+
     // Memoizer for usage in other recursive algorithms
     function memoizer(memo, formula) {
         var recur = function(n) {
@@ -73,24 +164,7 @@ var Main = (function() {
     var factorialShortMemo = memoizer([1, 1], function(recur, n) {
         return n * recur(n - 1);
     });
-    // Palindrome checker
-    function palindrome(str) {
-        newstr = str.toLowerCase();
-        if (newstr[0] !== newstr.slice(-1)) {
-            return false;
-        } else {
-            return true;
-        }
-        return palindrome(newstr[0]) + palindrome(newstr[newstr.slice(-1)]);
-    }
-    // Tower of Hanoi solver
-    function hanoi(disk, src, aux, dst) {
-        if (disk > 0) {
-            hanoi(disk - 1, src, dst, aux);
-            console.log("Move disk " + disk + " from " + src + " to " + dst);
-            hanoi(disk - 1, aux, src, dst);
-        }
-    }
+
     // Linked list
     var firstNode = {
         data: 0,
@@ -162,16 +236,7 @@ var Main = (function() {
             }
         }
     };
-    // Linear search
-    function linearSearch(array, target) {
-        var len = array.length;
-        for (var i = 0; i < len; i++) {
-            if (array[i] === target) {
-                return i;
-            }
-        }
-        return -1;
-    }
+
     // Array flattener
     function flattenArrays(a, r) {
         var len = a.length;
@@ -188,7 +253,7 @@ var Main = (function() {
         return r;
     }
     // Find maximum triplet, from Codility
-    function solution(A) {
+    function triplets(A) {
         // A is an array
         var len = A.length;
         Array.prototype.max = function() {
@@ -204,45 +269,5 @@ var Main = (function() {
         A = A.sort(sorter);
         var result = [A[0] * A[1] * A[len - 1], A[len - 1] * A[len - 2] * A[len - 3]].max();
         return result;
-    }
-
-    function mergeSort(array, start, end) {
-        if (start < end) {
-            var mid = Math.floor((start + end) / 2);
-            mergeSort(array, start, mid);
-            mergeSort(array, mid + 1, end);
-            merge(array, start, mid, end); // Implement merge
-        }
-
-        // Merge, which performs most of the work in merge sort
-        function merge(left, right) {
-            var result = [],
-                indexLeft = 0,
-                indexRight = 0,
-                leftLength = left.length,
-                rightLength = right.length;
-
-            while (indexLeft < leftLength && indexRight < rightLength) {
-                if (left[indexLeft] < right[indexRight]) {
-                    result.push(left[indexLeft++]);
-                } else {
-                    result.push(right[indexRight++]);
-                }
-            }
-            return result.concat(left.slice(indexLeft).concat(right.slice(indexRight)));
-        }
-        // Merge sort, recursive sorting algorithm with guaranteed n log n performance
-        function mergeSort(array) {
-            var len = array.length;
-            if (len < 2) {
-                return array;
-            }
-
-            var middle = Math.floor(len / 2),
-                left = array.slice(0, middle),
-                right = array.slice(middle);
-
-            return merge(mergeSort(left), mergeSort(right));
-        }
     }
 })();
