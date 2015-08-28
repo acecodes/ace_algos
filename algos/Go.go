64fb9e33bb72f7b30ec0587e84a43a954300b244
Ace <ace.github@gmail.com>
2015-08-28 16:03:32 -0700

    Added recursive summing function to Go

--- algos/Go.go
+++ algos/Go.go
@@ -11,6 +11,15 @@ func factorial(n uint) uint {
 	return n * factorial(n-1)
 }
 
+func summer(n uint) uint {
+	if n <= 1 {
+		return n
+	} else {
+		return n + summer(n-1)
+	}
+}
+
 func main() {
 	fmt.Println(factorial(7)) // 5040
+	fmt.Println(summer(5))    // 15
 }
