dd04fa73edcb9175fc0f11d1cda0cafad6500c39
Ace <ace.github@gmail.com>
2015-08-29 10:37:48 -0700

    Recursive power function for Go

--- algos/Go.go
+++ algos/Go.go
@@ -19,7 +19,16 @@ func summer(n uint) uint {
 	}
 }
 
+func powers(n uint, power uint) uint {
+	if power > 0 {
+		return n * powers(n, power-1)
+	} else {
+		return 1
+	}
+}
+
 func main() {
 	fmt.Println(factorial(7)) // 5040
 	fmt.Println(summer(5))    // 15
+	fmt.Println(powers(5, 2)) // 25
 }
