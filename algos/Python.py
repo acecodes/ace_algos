6f331dbb0ebc10cf861901e177ea99dbc124b265
Ace <ace.github@gmail.com>
2015-09-20 10:12:11 -0700

    Dijkstra's algorithm in Python

--- algos/Python.py
+++ algos/Python.py
@@ -1,12 +1,16 @@
 # Compatability imports that allow usage in Python 2 & 3
 from __future__ import print_function
 from timeit import Timer
+
 import string
 import operator
 import collections
 import math
+
+# Third-party libraries
 # import mmh3
 # import bitarray
+
 
 class Exceptions:
 
@@ -1410,6 +1414,48 @@ class Sorting(Algorithms):
         return right_mark
 
 
+inf = float('inf')
+Edge = collections.namedtuple('Edge', 'start, end, cost')
+
+
+class Dijkstra:
+    """
+    Dijkstra's algorithm implementation
+    Credit to RosettaCode
+    """
+
+    def __init__(self, edges):
+        self.edges = edges_2 = [Edge(*edge) for edge in edges]
+        self.vertices = set(sum(([e.start, e.end] for e in edges_2), []))
+
+    def dijkstra(self, source, dest):
+        assert source in self.vertices
+        dist = {vertex: inf for vertex in self.vertices}
+        previous = {vertex: None for vertex in self.vertices}
+        dist[source] = 0
+        q = self.vertices.copy()
+        neighbors = {vertex: set() for vertex in self.vertices}
+        for start, end, cost in self.edges:
+            neighbors[start].add((end, cost))
+
+        while q:
+            u = min(q, key=lambda vertex: dist[vertex])
+            q.remove(u)
+            if dist[u] == inf or u == dest:
+                break
+            for v, cost in neighbors[u]:
+                alt = dist[u] + cost
+                if alt < dist[v]:
+                    dist[v] = alt
+                    previous[v] = u
+        s, u = collections.deque(), dest
+        while previous[u]:
+            s.appendleft(u)
+            u = previous[u]
+        s.appendleft(u)
+        return s
+
+
 class Generation(Algorithms):
 
     """Luhn's algorith, for generating and validating number sequences
@@ -1765,6 +1811,19 @@ if __name__ == '__main__':
     # memo = {}
     # print(Math.Fibonacci_memo(9, memo))
 
-    """Factorial with memoization"""
-    memo2 = {}
-    print(Math.factorial_memo(5, memo2))
+    # """Factorial with memoization"""
+    # memo2 = {}
+    # print(Math.factorial_memo(5, memo2))
+
+    """Finding the minimal cost of traversing a graph with Dijkstra's algorithm"""
+    dijkstra = Dijkstra([
+        ('a', 'b', 7),
+        ('a', 'c', 9),
+        ('a', 'f', 14),
+        ('b', 'c', 10),
+        ('b', 'd', 15),
+        ('c', 'd', 11),
+        ('c', 'f', 2),
+        ('d', 'e', 6),
+        ('e', 'f', 9)])
+    print(dijkstra.dijkstra('a', 'f'))
