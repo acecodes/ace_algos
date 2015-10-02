8ef9608ea0b15da6d4efffd2a69a519e15981851
Ace <ace.github@gmail.com>
2015-10-02 08:35:51 -0700

    Added custom dictionary data structure

--- algos/Python.py
+++ algos/Python.py
@@ -186,6 +186,22 @@ class Hash(Algorithms):
 
 class DataStructures(Algorithms):
 
+    class StrKeyDict(collections.UserDict):
+        """
+        Dictionary that coerces non-string keys into strings
+        """
+
+        def __missing__(self, key):
+            if isinstance(key, str):
+                raise KeyError(key)
+            return self[str(key)]
+
+        def __contains__(self, key):
+            return str(key) in self.data
+
+        def __setitem__(self, key, item):
+            self.data[str(key)] = item
+
     class BloomFilter:
         """
         A simple Bloom Filter implementation
