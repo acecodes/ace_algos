aa494158287e7a63dff8768a0f86c875b2fcc8ee
Ace <ace.github@gmail.com>
2015-10-03 15:15:35 -0700

    First dip into neural networks

---
+++ ml/neural_networks.py
@@ -0,0 +1,29 @@
+from pybrain.tools.shortcuts import buildNetwork
+from pybrain.datasets import SupervisedDataSet
+
+# New network with 2 inputs, 3 hidden layers and 1 output
+ann = buildNetwork(2, 3, 1)
+
+# Data set with two dimensional inputs and one output
+data_set = SupervisedDataSet(2, 1)
+
+# Add 4 samples to network (XOR)
+data_set.addSample((0, 0), (0,))
+data_set.addSample((0, 1), (1,))
+data_set.addSample((1, 0), (1,))
+data_set.addSample((1, 1), (0,))
+
+# Print out all inputs and outputs
+for inpt, output in data_set:
+    print(inpt, output)
+
+# Print individual sets of the data
+print('Inputs: {}'.format(data_set['input']))
+print('Outputs: {}'.format(data_set['target']))
+
+# Let's clear the data and see what it looks like...
+print('Clearing data set...')
+data_set.clear()
+
+print('Inputs (cleared): {}'.format(data_set['input']))
+print('Outputs: {}'.format(data_set['target']))
