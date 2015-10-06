a671a516e1a7203ce727d0239bd17e4b7c7486c1
Ace <ace.github@gmail.com>
2015-10-06 06:04:41 -0500

    More neural network code

--- ml/neural_networks.py
+++ ml/neural_networks.py
@@ -1,7 +1,7 @@
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.datasets import SupervisedDataSet
 from pybrain.supervised.trainers import BackpropTrainer
-from pybrain.structure import FeedForwardNetwork, LinearLayer, SigmoidLayer, TanhLayer
+from pybrain.structure import FeedForwardNetwork, LinearLayer, SigmoidLayer, TanhLayer, FullConnection
 
 # New network with 2 inputs, 3 hidden layers and 1 output
 ann = buildNetwork(2, 3, 1)
@@ -24,8 +24,8 @@ print('Inputs: {}'.format(data_set['input']))
 print('Outputs: {}'.format(data_set['target']))
 
 # Let's clear the data and see what it looks like...
-print('Clearing data set...')
-data_set.clear()
+# print('Clearing data set...')
+# data_set.clear()
 
 print('Inputs (cleared): {}'.format(data_set['input']))
 print('Outputs: {}'.format(data_set['target']))
@@ -41,7 +41,7 @@ trainer.train()
 trainer.trainUntilConvergence()
 
 # Create a feedforward (one direction) network
-ff_net = FeedForwardNetwork
+ff_net = FeedForwardNetwork()
 input_layer = LinearLayer(2)
 hidden_layer = SigmoidLayer(3)
 output_layer = LinearLayer(1)
@@ -49,3 +49,15 @@ output_layer = LinearLayer(1)
 ff_net.addInputModule(input_layer)
 ff_net.addModule(hidden_layer)
 ff_net.addOutputModule(output_layer)
+
+# Create explicit connections between layers
+input_to_hidden = FullConnection(input_layer, hidden_layer)
+hidden_to_output = FullConnection(hidden_layer, output_layer)
+
+ff_net.addConnection(input_to_hidden)
+ff_net.addConnection(hidden_to_output)
+
+# Sort for proper network operation
+ff_net.sortModules()
+
+print(ff_net)
