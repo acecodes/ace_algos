bb89973dd78cd202aff3054321c5e22d603c64ad
Ace <ace.github@gmail.com>
2015-10-22 10:01:31 -0700

    More Pandas stuff

--- data_science/data_science.py
+++ data_science/data_science.py
@@ -42,3 +42,21 @@ print('Dataframe stats:')
 print(dataframe_2.describe())
 print('Dataframe transposition:')
 print(dataframe_2.T)
+
+test_index = pd.Series(np.random.randn(5), index=['a', 'b', 'c', 'd', 'e'])
+
+print('Another series (test_index):')
+print(test_index)
+
+print('Index of test_index:')
+print(test_index.index)
+
+print('Create series from a dictionary:')
+dic1 = {'a': 1, 'b': 2, 'c': 3}
+dict_series = pd.Series(dic1)
+print(dict_series)
+
+print('Series from dictionary with an arbitrary index:')
+dict_series_2 = pd.Series(dic1, index=['a', 'b', 'c', 'd'])
+print(dict_series_2)
+print('NaN is standard in Pandas for missing data!')
